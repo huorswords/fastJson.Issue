840cb261adb7f62b1d68f05dcc8f42e0e91985b5
Angel.G <agarcia@pasiona.com>
2015-06-16 13:07:00 +0200

    Reproduced issue on deserializing json data to IDdSet<>

---
+++ fastJSON.Issue/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿namespace fastJSON.Issue
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Data.Entity;
+    using System.Linq;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            PersonContext context =
+                fastJSON.JSON.ToObject<PersonContext>(System.IO.File.OpenText(@"Resources\json1.json").ReadToEnd());
+            Assert.AreEqual(((IQueryable<Person>)context.Persons).Count(), 3);
+        }
+
+        public class PersonContext
+        {
+            public IDbSet<Person> Persons { get; set; }
+        }
+
+        public class Person
+        {
+            public string FirstName { get; set; }
+            public string Surname { get; set; }
+        }
+    }
+}
